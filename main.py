ae64e08b0c4de9aa72ea02cd458c2266c16b9e37
aryan-vasudevan <vasudevan.aryan@gmail.com>
2025-07-02 11:39:42 -0400

    Quit on q.

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ API_KEY = os.getenv("API_KEY")
 WORKSPACE_NAME = os.getenv("WORKSPACE_NAME")
 WORKFLOW_ID = os.getenv("WORKFLOW_ID")
 
-def my_sink(result, video_frame):
+pipeline = None
+
+def process(result, video_frame):
     if result.get("keypoint_visualization"):
         cv2.imshow("Workflow Image", result["keypoint_visualization"].numpy_image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            pipeline.terminate()
 
-
-# Predict off live footage with inference pipeline
+# Initialize and start the pipeline
 pipeline = InferencePipeline.init_with_workflow(
     api_key=API_KEY,
     workspace_name=WORKSPACE_NAME,
     workflow_id=WORKFLOW_ID,
     video_reference=0,
     max_fps=30,
-    on_prediction=my_sink
+    on_prediction=process
 )
+
 pipeline.start()
 pipeline.join()
+cv2.destroyAllWindows()
